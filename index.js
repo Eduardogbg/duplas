aecf0ada50527d57e046731eb6f5e0a944d60a37
Eduardo <eduardogbg@gmail.com>
2020-03-18 05:12:19 -0300

    Log errors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const commandHandler = require('./src/commandHandler');
 
+const ERROR_CHANNEL = '689747252562231306';
 const { TOKEN } = process.env;
 
 const bot = new Discord.Client();
@@ -18,6 +19,7 @@ bot.on('message', async message => {
       await commandHandler(bot, message);
     }
   } catch (err) {
+    bot.channels.cache.get(ERROR_CHANNEL).send(`Erro: ${err.message}`);
     console.error("Erro no message listener", err);
   } 
 });
